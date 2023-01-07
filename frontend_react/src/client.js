cec61f91dba90ae34d38940dd3150e48999de1ad
Lucas Braida <lucbnaza@gmail.com>
2023-01-07 18:34:22 -0300

    Adding react-dotenv

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -1,12 +1,12 @@
 import sanityClient from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
-const {REACT_APP_SANITY_PROJECT_ID, REACT_APP_SANITY_TOKEN}  = require('./config.json')
+import env from 'react-dotenv'
 export const client = sanityClient({
-    projectId: REACT_APP_SANITY_PROJECT_ID,
+    projectId: env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
-    token:  REACT_APP_SANITY_TOKEN
+    token:  env.REACT_APP_SANITY_TOKEN
 })
 
 const builder = imageUrlBuilder(client)
