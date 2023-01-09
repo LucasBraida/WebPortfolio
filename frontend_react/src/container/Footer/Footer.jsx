f67af88f90a034ab2f838e98d0267766e7f3d1f2
Lucas Braida <lucbnaza@gmail.com>
2023-01-08 21:05:55 -0300

    Contact changes

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -5,6 +5,9 @@ import { AppWrap, MotionWrap } from '../../wrapper'
 import { client } from '../../client'
 import './Footer.scss'
 
+import resume from '../../assets/Resume-Lucas-Braida.pdf'
+import resumePNG from '../../assets/resume.png'
+
 const Footer = () => {
   const [formData, setFormData] = useState({ name: '', message: '', email: '' })
   const [isFormSubmitted, setIsFormSubmitted] = useState(false)
@@ -48,23 +51,12 @@ const Footer = () => {
           <img src={images.mobile} alt='mobile' />
           <a href='tel:+55 (32) 99147-8312' className='p-text'>+55 (32) 99147-8312</a>
         </div>
+        <div className='app__footer-card'>
+          <img src={resumePNG} alt='mobile' />
+          <a href={resume} download='resume_Lucas_Braida.pdf' className='p-text'>My resume</a>
+        </div>
       </div>
 
-      {!isFormSubmitted ?
-        <div className='app__footer-form app__flex'>
-          <div className='app__flex'>
-            <input className='p-text' type='text' placeholder='Your name' value={name} onChange={handleChangeInput} name='name' />
-          </div>
-          <div className='app__flex'>
-            <input className='p-text' type='email' placeholder='Your email' value={email} onChange={handleChangeInput} name='email' />
-          </div>
-          <div className='app__flex'>
-            <textarea className='p-text' placeholder='Your Message' value={message} name='message' onChange={handleChangeInput} />
-          </div>
-          <button type='button' className='p-text' onClick={handleSubmit}>{loading ? "Sending" : 'Send Message'}</button>
-        </div>
-        : <div>
-          <h3 className='head-text'>Thank you for getting in touch</h3></div>}
     </>
   )
 }
